d126e6be5c275072772988c69914cbda57c65588
Inna <shcipchik.inna@gmail.com>
2025-04-18 16:02:29 +0300

    updated index.js, add setInterval

--- index.js
+++ index.js
@@ -4,44 +4,38 @@ const API_KEY = CONFIG.API_KEY;
 
 const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 
-const newList = document.getElementById("news-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const checkInterval = setInterval(() => {
+    const newList = document.getElementById("news-list");
 
-fetch(url)
-  .then((repsonse) => repsonse.json())
-  .then((data) => {
-    console.log(data.articles);
+    if (newList) {
+      clearInterval(checkInterval);
 
-    data.articles.forEach((article) => {
-      const newItem = document.createElement("li");
+      fetch(url)
+        .then((repsonse) => repsonse.json())
+        .then((data) => {
+          console.log(data.articles);
 
-      const date = new Date(article.publishedAt);
-      const timeString = date.toLocaleTimeString("uk-UA", {
-        hour: "2-digit",
-        minute: "2-digit"
-      });
+          data.articles.forEach((article) => {
+            const newItem = document.createElement("li");
 
-      newItem.innerHTML = `
+            const date = new Date(article.publishedAt);
+            const timeString = date.toLocaleTimeString("uk-UA", {
+              hour: "2-digit",
+              minute: "2-digit"
+            });
+
+            newItem.innerHTML = `
         <a href = "${article.url}" target = "_blank">
         <time>${timeString}</time>
         <h3>${article.title}</h3> 
         <span/>
         </a>
       `;
-      newList.appendChild(newItem);
-    });
-  })
-  .catch((error) => console.error("Error", error));
-
-let favoriteMovies = [];
-
-//  пошук та маніпуляції з масивом
-const id = "some-unique-id"; // отримання id з елемента
-const index = favoriteMovies.indexOf(id);
-
-if (index !== -1) {
-  // Якщо id знайдено, видаляємо його з масиву
-  favoriteMovies.splice(index, 1);
-} else {
-  // Якщо id не знайдено, додаємо його до масиву
-  favoriteMovies.push(id);
-}
+            newList.appendChild(newItem);
+          });
+        })
+        .catch((error) => console.error("Error", error));
+    }
+  }, 100);
+});
